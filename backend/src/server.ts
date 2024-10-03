b4b9293453e9e314804f59719a0a4f4d950c63bb
Evgenii Karpov <one1two215@gmail.com>
2024-10-03 17:58:15 +0200

    Updated server side

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata"
 import express from "express"
+import cors from "cors"
 import AppDataSource from "./data-source"
 import battlepassRouter from "./routes/battlepass"
 import profileRouter from "./routes/profile"
@@ -8,6 +9,7 @@ import authRouter from "./routes/auth"
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use("/api", battlepassRouter)
 app.use("/api", profileRouter)
 app.use("/api", authRouter)
