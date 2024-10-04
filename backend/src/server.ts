23811f30bf45ea8c7634009ede80138d1925cad5
Evgenii Karpov <one1two215@gmail.com>
2024-10-04 18:41:14 +0200

    deploy to heroku

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,6 +5,7 @@ import AppDataSource from "./data-source"
 import battlepassRouter from "./routes/battlepass"
 import profileRouter from "./routes/profile"
 import authRouter from "./routes/auth"
+import path from "path"
 
 const app = express()
 
@@ -13,6 +14,10 @@ app.use(cors())
 app.use("/api", battlepassRouter)
 app.use("/api", profileRouter)
 app.use("/api", authRouter)
+app.use(express.static(path.join(__dirname, "public")))
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"))
+})
 
 app.get("/health", (req, res) => {
     res.status(200).send("OK")
